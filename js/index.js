270356458cd2c3dfc391015e925bc00eb06ddc97
Minghsun <minghsun_lee@trend.com.tw>
2015-10-24 21:08:27 +0800

    support responsive

--- js/index.js
+++ js/index.js
@@ -10,4 +10,27 @@ $(function(){
 	$('.demo-video').on('ended',function(){
 		$('.play-video-mask').show();
 	})
+
+	$('.play-video-wrapper__m').on('click',function(){
+		$('.mask').show();
+		$('.demo-video__m')[0].play();
+		location.hash="videoplaying"
+	})
+
+	$('.demo-video__m').on('click',function(e){
+		e.stopPropagation();
+		$('.demo-video__m')[0].play();
+	})
+
+	$('.mask').on('click',function(e){
+		$(this).hide();
+		$('.demo-video__m')[0].pause();
+		location.hash=""
+	});
+
+	window.onhashchange = function() {
+		if(location.hash.indexOf("videoplaying")==-1 && $('.mask').css('display')!='none'){
+			$('.mask').hide();	
+		}
+	}
 });
